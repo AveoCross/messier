2d2c5e79acba3fd7cacc92619824d4c6d9d768be
Aveo <kzyskowski7@gmail.com>
2018-04-24 06:14:31 +0200

    Script files reviewed and changed - tar to zip

--- backup.sh
+++ backup.sh
@@ -2,12 +2,12 @@
 #Purpose = Minecraft Server Backup
 #Created on 23-1-2017
 #Author = Aveo
-#Version 1.0
+#Version 1.2
 #START
 TIME=`date +%d-%m-%y`
-FILENAME=Backup-$TIME.tar.gz
-SRCDIR=/home/aveo/crowcraft
-DESDIR=/home/aveo/crowcraftbackup
+FILENAME=Backup-$TIME.zip
+SRCDIR=/home/aveo/ravencraft/ravencraft_save
+DESDIR=/home/aveo/ravencraftbackup
 
-tar -cvpzf $DESDIR/$FILENAME $SRCDIR 
+zip -r $DESDIR/$FILENAME $SRCDIR 
 #END
